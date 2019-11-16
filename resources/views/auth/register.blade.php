6ee5b224c98af27816911d2275f01df0ae1225af
momoko.shirai <mona11020325@gmail.com>
2019-11-16 13:58:40 +0000

    user login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,13 +21,13 @@
             </div>
             
             <div class="form-group">
-               {!! Form::label('passwod', 'Password') !!}
+               {!! Form::label('password', 'Password') !!}
                {!! Form::password('password', ['class' => 'form-control']) !!}
             </div>
             
             <div class="form-group">
-               {!! Form::label('password_confimation', 'Confimation') !!}
-               {!! Form::password('password_confimation', ['class' => 'form-control']) !!}
+               {!! Form::label('password_confirmation', 'Confirmation') !!}
+               {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
             </div>
             
             {!! Form::submit('Sign up', ['class' => 'btn btn-primary btn-block']) !!}
